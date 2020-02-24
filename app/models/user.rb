1748710fce32d50d87a5eecdee4d20194f37f827
Stephano Wurttele <stephano.wurttele@utec.edu.pe>
2020-02-24 17:13:20 +0000

    Done users controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  has_many: cars
-  has_many: bookings, through: cars
+  has_many :cars
+  has_many :bookings, through: :cars
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :username, existance: true, uniqueness: true
-  validates :name, existance: true
+  validates :username, presence: true, uniqueness: true
+  validates :name, presence: true
 
 end
