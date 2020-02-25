732ad0b71fd23fb2c27bb08fc977de979147d6bd
Stephano Wurttele <stephano.wurttele@utec.edu.pe>
2020-02-25 21:07:44 +0000

    Add dotenv - Protect my secret data in .env file. Also fixed seed

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.5'
 
+gem 'dotenv-rails', groups: [:development, :test]
+
 #Seeds
 gem 'faker'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,15 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 user1 = User.create!(username: "stephanoFW", name: "Stephano", email: "stephanoFW@gmail.com", password:"123456", password_confirmation:"123456")
-car1 = Car.create!(description: "Nice car 1", price: 160, model: Faker::Vehicle.model, kind: "electric", brand: Faker::Vehicle.manufacture, user: user1)
+car1 = Car.create!(description: "Nice car 1", price: 160, model: Faker::Vehicle.model, kind: "Electric", brand: Faker::Vehicle.manufacture, user: user1)
 car1.reviews.create!(rating: 5, content: 'pretty damn good')
 
 user2 = User.create!(username: "MaxD", name: "Max", email: "max.t.denton@gmail.com", password:"123456", password_confirmation:"123456")
-car2 = Car.create!(description: "Nice car 2", price: 170, model: Faker::Vehicle.model, kind: "electric", brand: Faker::Vehicle.manufacture, user: user1)
+car2 = Car.create!(description: "Nice car 2", price: 170, model: Faker::Vehicle.model, kind: "Electric", brand: Faker::Vehicle.manufacture, user: user1)
 car2.reviews.create!(rating: 4, content: 'damn good')
 
 user3 = User.create!(username: "MaxV", name: "Stephano", email: "max.v@gmail.com", password:"123456", password_confirmation:"123456")
-car3 = Car.create!(description: "Nice car 3", price: 180, model: Faker::Vehicle.model, kind: "electric", brand: Faker::Vehicle.manufacture, user: user2)
+car3 = Car.create!(description: "Nice car 3", price: 180, model: Faker::Vehicle.model, kind: "Electric", brand: Faker::Vehicle.manufacture, user: user2)
 car3.reviews.create!(rating: 3, content: 'good')
 
 booking1 = Booking.create!(car: car1, user: user2)
