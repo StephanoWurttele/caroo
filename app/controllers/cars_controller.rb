4f1ebb60682ee924ba78723c2cb9175cd7570316
Stephano Wurttele <stephano.wurttele@utec.edu.pe>
2020-02-25 14:57:00 +0000

    Done editing and deleting of cars

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,9 +1,9 @@
 class CarsController < ApplicationController
-
+  before_action :define_kinds, only: [:edit, :new]
+  before_action :get_car, only: [:show, :edit, :update, :destroy]
   def index
     @cars = Car.all
     @search = params["search"]
-
     if @search.present?
       @brand = @search[:brand]
       @cars = Car.where("brand ILIKE ?", @brand)
@@ -11,25 +11,45 @@ class CarsController < ApplicationController
   end
 
   def show
-    @car = Car.find(params[:id])
   end
 
   def new
-    @car = Car.new(car_params)
+    @car = Car.new
   end
 
   def create
-    if @car = Car.create(car_params)
+    @car = Car.new(car_params)
+    @car.user = current_user
+    if @car.save!
       redirect_to car_path(@car)
     else
       render :new
     end
   end
 
+  def edit
+  end
 
+  def update
+    @car.update(car_params)
+    redirect_to car_url(@car)
+  end
+
+  def destroy
+    @car.delete
+    redirect_to cars_url
+  end
   private
 
   def car_params
-    params.require(:car).permit(:description, :price, :model, :type, :brand)
+    params.require(:car).permit(:description, :price, :model, :kind, :brand)
+  end
+
+  def define_kinds
+    @kinds = ['Petrol', 'Diesel', 'Electric', 'Hybrid']
+  end
+
+  def get_car
+    @car = Car.find(params[:id])
   end
 end
--- app/models/car.rb
+++ app/models/car.rb
@@ -2,13 +2,15 @@ class Car < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many :reviews,dependent: :destroy
   belongs_to :user
+  KINDS = ['Petrol', 'Diesel', 'Electric', 'Hybrid']
   validates :model, presence: true
   validates :brand, presence: true
-  validates :kind, presence: true
+  validates :kind, presence: true, inclusion: {in: KINDS}
   validates :price, presence: true
   validates :description, presence: true
 
-  def show
-    "#{self.kind.capitalize} #{self.model}. #{self.brand}. Owner: #{self.user.username}"
+  def info
+    "#{self.kind.capitalize} #{self.model}. #{self.brand}."
   end
+
 end
