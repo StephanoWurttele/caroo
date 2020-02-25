cf378d4f3a869aa12fd6978dd8f2d7b4ed9e2e69
daniel ghimpu <danielghimpu@yahoo.com>
2020-02-25 10:43:56 +0000

    reviews model and controller done,also new action on views

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,30 @@
 class ReviewsController < ApplicationController
+
+  def index
+    @car = Car.find(params[:car_id])
+    @reviews = Review.all
+  end
+
   def new
     @car = Car.find(params[:car_id])
     @review = Review.new
   end
+
+  def create
+    @review = Review.new(review_params)
+    @car = Car.find(params[:car_id])
+    @review.car = @car
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content)
+  end
+
 end
