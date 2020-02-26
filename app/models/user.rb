f512b5c15b5c478ef2f19c7e8feb539daca9d31d
Stephano Wurttele <stephano.wurttele@utec.edu.pe>
2020-02-26 14:41:42 +0000

    Done pp

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :cars
   has_many :bookings, through: :cars
+  has_one_attached :photo
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :username, presence: true, uniqueness: true
