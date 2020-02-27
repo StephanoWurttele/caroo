4f9cda7999fa09cce0f1304574efae07b472aed1
daniel ghimpu <danielghimpu@yahoo.com>
2020-02-27 12:58:11 +0000

    Added equipment to the cars

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_27_110923) do
+ActiveRecord::Schema.define(version: 2020_02_27_123709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,10 @@ ActiveRecord::Schema.define(version: 2020_02_27_110923) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "seats"
+    t.integer "doors"
+    t.string "transmission"
+    t.integer "year"
     t.index ["user_id"], name: "index_cars_on_user_id"
   end
 
