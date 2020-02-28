932e1b3080d00d2c295cf734be5663881e169cb2
Stephano Wurttele <stephano.wurttele@utec.edu.pe>
2020-02-28 19:21:12 +0000

    Login and out to same page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
 before_action :configure_permitted_parameters, if: :devise_controller?
+before_action :store_user_location!, if: :storable_location?
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
@@ -7,5 +8,24 @@ before_action :configure_permitted_parameters, if: :devise_controller?
 
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :username])
+  end
+
+private
+
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr? 
+  end
+
+  def store_user_location!
+    # :user is the scope we are authenticating
+    store_location_for(:user, request.fullpath)
+  end
+
+  def after_sign_in_path_for(resource_or_scope)
+    stored_location_for(resource_or_scope) || super
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    request.referrer
   end
 end
