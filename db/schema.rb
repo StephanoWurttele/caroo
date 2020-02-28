6e1fcb45e9d2d29e87a778f2c2300cc70788ece5
Stephano Wurttele <stephano.wurttele@utec.edu.pe>
2020-02-28 16:50:54 +0000

    Done

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_28_161022) do
-
+ActiveRecord::Schema.define(version: 2020_02_28_115202) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,7 +87,6 @@ ActiveRecord::Schema.define(version: 2020_02_28_161022) do
     t.string "name"
     t.boolean "owner"
     t.string "username"
-
     t.float "latitude"
     t.float "longitude"
     t.string "address"
